e1e66daee6e4a11e1754a537e8be449ed05e06fa
Josh <joshuabecconsall05@gmail.com>
2021-07-11 19:35:45 +0100

    initial commit

---
+++ src/main/java/me/josh/nightvision/NightVision.java
@@ -0,0 +1,17 @@
+package me.josh.nightvision;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class NightVision extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
